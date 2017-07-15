a312e6ff8e571d2d98b130ad8c266addb42dff47
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-07-15 16:22:08 +0530

    require explicit `entry` and `output` paths for encoding HTML files

--- src/encoder.js
+++ src/encoder.js
@@ -1,7 +1,6 @@
 // @flow
 
 import fs from 'fs';
-import path from 'path';
 
 import utils from './utils';
 
@@ -15,16 +14,31 @@ class Encoder {
   mapper: Mapper;
   targetFontFilenames: Array<string>;
 
+  protextStyleRegexp: RegExp;
+  sourceTextRegexp: RegExp;
+
   encodeText: Function;
   encodeHtmlFile: Function;
   encodeHtmlStream: Function;
+  sourceTextReplacer: Function;
 
   constructor(options: EncoderOptions) {
     this.unpackOptions(options);
 
+    this.protextStyleRegexp = new RegExp('{{protext}}', 'g');
+    this.sourceTextRegexp = new RegExp(
+      '{{#protext}}([\\s\\S]*?){{/protext}}',
+      'g',
+    );
+
     this.encodeText = this.encodeText.bind(this);
     this.encodeHtmlFile = this.encodeHtmlFile.bind(this);
     this.encodeHtmlStream = this.encodeHtmlStream.bind(this);
+    this.sourceTextReplacer = this.sourceTextReplacer.bind(this);
+  }
+
+  sourceTextReplacer(_: any, p1: string) {
+    return `<span class="protext">${this.encodeText(p1)}</span>`;
   }
 
   unpackOptions(options: EncoderOptions) {
@@ -35,68 +49,45 @@ class Encoder {
   }
 
   encodeBody(html: string): string {
-    const sourceTextRegexp = new RegExp(
-      '{{#protext}}([\\s\\S]*?){{/protext}}',
-      'g',
-    );
-
+    console.log(this.sourceTextReplacer);
     const encodedHtml = html.replace(
-      sourceTextRegexp,
-      (_, p1) => `<span class="protext">${this.encodeText(p1)}</span>`,
+      this.sourceTextRegexp,
+      this.sourceTextReplacer,
     );
 
     return encodedHtml;
   }
 
   encodeHead(html: string): string {
-    const protextStyleRegexp = new RegExp('{{protext}}', 'g');
-
     const encodedHtml = html.replace(
-      protextStyleRegexp,
+      this.protextStyleRegexp,
       utils.getStyleTag(this.targetFontFilenames, this.fontFamily),
     );
 
     return encodedHtml;
   }
 
-  encodeHtmlFile(filepath: string) {
-    const filename = path.basename(filepath).replace(/(.tmpl)?$/, '');
-
-    let html = fs.readFileSync(filepath, 'utf8');
+  encodeHtmlFile(entry: string, output: string) {
+    let html = fs.readFileSync(entry, 'utf8');
 
     html = this.encodeHead(html);
     html = this.encodeBody(html);
 
-    fs.writeFileSync(path.resolve(this.destination, filename), html);
+    fs.writeFileSync(output, html);
   }
 
-  encodeHtmlStream(filepath: string) {
-    const filename = path.basename(filepath).replace(/(.tmpl)?$/, '');
-
-    const protextStyleRegexp = new RegExp('{{protext}}', 'g');
-    const sourceTextRegexp = new RegExp(
-      '{{#protext}}([\\s\\S]*?){{/protext}}',
-      'g',
-    );
-
-    const inStream = fs.createReadStream(filepath);
-    const outStream = fs.createWriteStream(
-      path.resolve(this.destination, filename),
-    );
+  encodeHtmlStream(entry: string, output: string) {
+    const inStream = fs.createReadStream(entry);
+    const outStream = fs.createWriteStream(output);
 
     inStream
       .pipe(
         replaceStream(
-          protextStyleRegexp,
+          this.protextStyleRegexp,
           utils.getStyleTag(this.targetFontFilenames, this.fontFamily),
         ),
       )
-      .pipe(
-        replaceStream(
-          sourceTextRegexp,
-          (_, p1) => `<span class="protext">${this.encodeText(p1)}</span>`,
-        ),
-      )
+      .pipe(replaceStream(this.sourceTextRegexp, this.sourceTextReplacer))
       .pipe(outStream);
   }
 
